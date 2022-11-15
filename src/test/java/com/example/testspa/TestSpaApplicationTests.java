104e4a7ca9b161fb617c5668bb3827e48f8dfb79
bitbytejoy <bitbytejoy@gmail.com>
2022-11-15 22:13:12 +0100

    BIG BANG

---
+++ src/test/java/com/example/testspa/TestSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
